df17601fd2d0b3535aa70f7d346b170e06a7f4f9
katemorkeski <kmorkeski@whoi.edu>
2026-09-01 15:30:34 -0400

    re-run papa 8-9-10 to get metadata

--- papa-08.R
+++ papa-08.R
@@ -1,51 +1,54 @@
-## Access OOI data from Alfresco
+#load required packages 
+library(tidyverse) 
+library(httr)
+library(readxl)
+library(here)
+library(janitor)
+
+#set path to root of project
+here("ooi-carbonate-setup")
+
+## Access OOI data 
 
 # read in URLs and get data files
 
 # Papa 8 Sampling Log Version 1-00
-log_url <- ('https://alfresco.oceanobservatories.org/alfresco/webdav/OOI/Global%20Station%20Papa%20Array/Cruise%20Data/Station_Papa-08_SKQ202111S_2021-07-18/Ship%20Data/Water%20Sampling/Station_Papa-08_SKQ202111S_CTD_Sampling_Log_2021-08-25_Ver_1-00.xlsx')
+log_url <- ('https://rawdata.oceanobservatories.org/files/cruise_data/Station_Papa/Station_Papa-08_SKQ202111S_2021-07-18/Water_Sampling/Station_Papa-08_SKQ202111S_CTD_Sampling_Log_2021-08-25_Ver_1-00.xlsx')
 
 # Papa 8 salinity data Version 1-00
-sal_url <- ('https://alfresco.oceanobservatories.org/alfresco/webdav/OOI/Global%20Station%20Papa%20Array/Cruise%20Data/Station_Papa-08_SKQ202111S_2021-07-18/Ship%20Data/Water%20Sampling/Station_Papa-08_SKQ202111S_Salinity_Sample_Data_2021-08-25_Ver_1-00.xlsx')
+sal_url <- ('https://rawdata.oceanobservatories.org/files/cruise_data/Station_Papa/Station_Papa-08_SKQ202111S_2021-07-18/Water_Sampling/Station_Papa-08_SKQ202111S_Salinity_Sample_Data_2021-08-25_Ver_1-00.xlsx')
 
-
-# Supply credentials for Alfresco
-creds <- read_file("authenticate.txt")
-
-httr::GET(log_url, authenticate(creds, creds), write_disk(logtf <- tempfile(fileext = ".xlsx")))
+httr::GET(log_url, write_disk(logtf <- tempfile(fileext = ".xlsx")))
 
 logtf
 
-httr::GET(sal_url, authenticate(creds, creds), write_disk(saltf <- tempfile(fileext = ".xlsx")))
+httr::GET(sal_url, write_disk(saltf <- tempfile(fileext = ".xlsx")))
 
 saltf
 
-
 # assign files to data frames
-
 log <- read_excel(logtf, 1L) 
 sal <- read_excel(saltf, 1L) 
 
-
 # count DICTA and pH bottles
-
 n_distinct(log$`DIC/TA Bottle #`, na.rm = TRUE)
 n_distinct(log$`pH Bottle #`, na.rm = TRUE)
 
-# format cruise ID and column headers
+log <- clean_names(log)
 
+# format cruise ID and column headers
 bottles <- log %>%
-  rename(Cruise_ID = "Cruise ID") %>%
-  rename(station = "Station-Cast #") %>%
-  rename(niskin = "Niskin #") %>%
-  rename(PH_bottle = "pH Bottle #") %>%
-  rename(DICTA_bottle = "DIC/TA Bottle #") %>%
-  rename(salt_bottle = "Salts Bottle #") 
+  rename(station = station_cast_number) %>%
+  rename(niskin = niskin_number) %>%
+  rename(PH_bottle = p_h_bottle_number) %>%
+  rename(DICTA_bottle = dic_ta_bottle_number) %>%
+  rename(salt_bottle = salts_bottle_number) |>
+  rename(depth = trip_depth_m)
 
 bottles$station <- as.numeric(bottles$station)
 bottles$niskin <- as.numeric(bottles$niskin)
 
-headers <- c("Cruise_ID", "station", "niskin", "Date", "PH_bottle", "DICTA_bottle", "salt_bottle")
+headers <- c("cruise_id", "date", "station", "niskin", "depth", "PH_bottle", "DICTA_bottle", "salt_bottle")
 bottles <- bottles[, headers]
 
 sal <- sal %>%
@@ -57,11 +60,9 @@ sal <- sal %>%
 sal$station <- as.numeric(sal$station)
 sal$niskin <- as.numeric(sal$niskin)
 
-# match salinity to cruise log
 # isolate carbonate rows
 # check number of bottles
-# join with salinity
-
+# match salinity to cruise log
 bottles <- filter(bottles, PH_bottle != "NA" | DICTA_bottle != "NA")
 
 n_distinct(bottles$DICTA_bottle, na.rm = TRUE)
@@ -69,15 +70,13 @@ n_distinct(bottles$PH_bottle, na.rm = TRUE)
 
 bottles_sal <- left_join(bottles, sal, c("station", "niskin", "salt_bottle"))
 
-
 # clean up salinity dataframe and write to csv
-
 bottles_sal$salinity_psu <- as.numeric(bottles_sal$salinity_psu)
 
 bottles_sal <- bottles_sal %>%
   mutate(salinity_psu = round(salinity_psu, 4))
 
-headers <- c("Cruise_ID", "Date", "DICTA_bottle", "PH_bottle", "salinity_psu")
+headers <- c("cruise_id", "date", "station", "niskin", "depth", "PH_bottle", "DICTA_bottle","salt_bottle","salinity_psu")
 bottles_sal <- bottles_sal[, headers]
 
 write_csv(bottles_sal, "Papa_08_carbonate_bottle_salinity.csv")
--- papa-09.R
+++ papa-09.R
@@ -7,15 +7,16 @@ n_distinct(bottles$`DIC/TA Bottle #`, na.rm = TRUE)
 n_distinct(bottles$`pH Bottle #`, na.rm = TRUE)
 
 # format columns
-
 bottles <- bottles %>%
   rename(Cruise_ID = "Cruise ID") %>%
   rename(station = "Station-Cast #") %>%
   rename(niskin = "Niskin #") %>%
+  rename(depth = "Trip Depth [m]") %>%
   rename(PH_bottle = "pH Bottle #") %>%
   rename(DICTA_bottle = "DIC/TA Bottle #") %>%
   rename(CTD_salinity_1 = "CTD Salinity 1") %>%
-  rename(CTD_salinity_2 = "CTD Salinity 2")
+  rename(CTD_salinity_2 = "CTD Salinity 2") |>
+  filter(PH_bottle != "NA" | DICTA_bottle != "NA")
 
 bottles$station <- as.numeric(bottles$station)
 bottles$niskin <- as.numeric(bottles$niskin)
@@ -23,11 +24,10 @@ bottles$CTD_salinity_1 <- as.numeric(bottles$CTD_salinity_1)
 bottles$CTD_salinity_2 <- as.numeric(bottles$CTD_salinity_2)
 
 bottles <- bottles %>%
-  mutate(CTD_salinity_avg = mean(c(CTD_salinity_1, CTD_salinity_2))) %>%
-  mutate(CTD_salinity_avg = round(CTD_salinity_avg, 4)) %>%
-  filter(PH_bottle != "NA" | DICTA_bottle != "NA")
-
-headers <- c("Cruise_ID", "station", "niskin", "Date", "PH_bottle", "DICTA_bottle", "CTD_salinity_1", "CTD_salinity_2", "CTD_salinity_avg")
+  mutate(CTD_salinity_avg = (CTD_salinity_1 + CTD_salinity_2)/2) |>
+  mutate(CTD_salinity_avg = round(CTD_salinity_avg, 4)) 
+  
+headers <- c("Cruise_ID", "station", "niskin", "depth", "Date", "PH_bottle", "DICTA_bottle", "CTD_salinity_1", "CTD_salinity_2", "CTD_salinity_avg")
 bottles <- bottles[, headers]
 # can use this for summary file after analysis
 
@@ -35,8 +35,7 @@ n_distinct(bottles$DICTA_bottle, na.rm = TRUE)
 n_distinct(bottles$PH_bottle, na.rm = TRUE)
 
 # clean up salinity dataframe and write to csv
-
-headers <- c("Cruise_ID", "Date", "DICTA_bottle", "PH_bottle", "CTD_salinity_1")
-bottles <- bottles[, headers]
+#headers <- c("Cruise_ID", "Date", "station", "niskin", "depth", "DICTA_bottle", "PH_bottle", "CTD_salinity_avg")
+#bottles <- bottles[, headers]
 
 write_csv(bottles, "Papa_09_carbonate_bottle_salinity.csv")
